368199b80c30c0a1a3a18411e9b49d668d070666
Ketan Patil <k2p1020@gmail.com>
2021-04-16 21:36:38 +0530

    V 1.2

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
 
-
+# list of attributes
 l1=['O-blood-group','child-adult','medical-complication','international-travel','contact-with-covid','covid-symptoms']
 
+# classes
 disease=['Very-high','Pretty-High','High','Pretty-low','low']
 
-# TESTING DATA df -------------------------------------------------------------------------------------
+# TESTING DATA df 
 df=pd.read_csv("dataset/test.csv")
 
 df.replace({'prognosis':{'Very-high':0,'Pretty-High':1,'High':2,'Pretty-low':3,'low':4}},inplace=True)
@@ -16,72 +17,73 @@ df.replace({'prognosis':{'Very-high':0,'Pretty-High':1,'High':2,'Pretty-low':3,'
 # print(df.head())
 
 X= df[l1]
-
-
 y = df[["prognosis"]]
 np.ravel(y)
-# print(y)
 
-# TRAINING DATA tr --------------------------------------------------------------------------------
+
+# TRAINING DATA tr 
 tr=pd.read_csv("dataset/train.csv")
 tr.replace({'prognosis':{'Very-high':0,'Pretty-High':1,'High':2,'Pretty-low':3,'low':4}},inplace=True)
 
 X_test= tr[l1]
 y_test = tr[["prognosis"]]
 np.ravel(y_test)
-# ------------------------------------------------------------------------------------------------------
+# --------------------
 
 # Flask constructor 
 app = Flask(__name__) 
 
+#Decision Tree Algorithm
 def DecisionTree(psymptoms):
-    
+    #import tree 
     from sklearn import tree
 
-    clf3 = tree.DecisionTreeClassifier()   # empty model of the decision tree
+    # empty model of the decision tree
+    clf3 = tree.DecisionTreeClassifier()  
     clf3 = clf3.fit(X,y)
 
-    # calculating accuracy-------------------------------------------------------------------
+    # calculating accuracy-----------------------
     from sklearn.metrics import accuracy_score
     y_pred=clf3.predict(X_test)
-    # print(accuracy_score(y_test, y_pred))
-    # print("Decision Tree Accuracy: ", accuracy_score(y_test, y_pred,normalize=False))
-    # -----------------------------------------------------
+    # --------------------------------------------
 
+    # Prediction
     inputtest = [psymptoms]
     predict = clf3.predict(inputtest)
     predicted=predict[0]
 
+    # check if prediction is done successfully 
     h='no'
-    for a in range(0,len(disease)):# assigning value for variable "a" while checking the output number exists line 20 
+    for a in range(0,len(disease)):
         if(predicted == a):
             h='yes' 
             break
         
     if (h=='yes'):
-        # print("COVID-19 Risk: ", disease[a])
         return(disease[a], accuracy_score(y_test, y_pred,normalize=False)) 
     else:
         return("Not Found", "Not Found")
 
-
+#Naive Bayes Algorithm
 def NaiveBayes(psymptoms):
+    # import gaussian
     from sklearn.naive_bayes import GaussianNB
+
+    # empty model of naive gaussian
     gnb = GaussianNB()
     gnb=gnb.fit(X,np.ravel(y))
 
-    # calculating accuracy-------------------------------------------------------------------
+    # calculating accuracy--------------------------
     from sklearn.metrics import accuracy_score
     y_pred=gnb.predict(X_test)
-    # print(accuracy_score(y_test, y_pred))
-    # print("Naive Bayes Accuracy: ", accuracy_score(y_test, y_pred,normalize=False))
-    # -----------------------------------------------------
+    # ----------------------------------------------
 
-
+    # Prediction
     inputtest = [psymptoms]
     predict = gnb.predict(inputtest)
     predicted=predict[0]
 
+    # check if prediction is done successfully 
     h='no'
     for a in range(0,len(disease)):
         if(predicted == a):
@@ -89,7 +91,6 @@ def NaiveBayes(psymptoms):
             break
 
     if (h=='yes'):
-        # print("COVID-19 Risk: ", disease[a])
         return(disease[a], accuracy_score(y_test, y_pred,normalize=False)) 
     else:
         return("Not Found", "Not Found")
@@ -102,6 +103,7 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == "POST":
+        # Take user inputs from html form
         name = request.form.get("name")
         age = int(request.form.get("age"))
         blood_group = int(request.form.get("blood_group"))
